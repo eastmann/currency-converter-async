32e8d4e89bb1132d1034c747a6cd7047e1ea0c26
Maxim Klochkov <m.klochkov@sap.com>
2020-11-16 14:55:00 +0300

    Initial setup

---
+++ currency-converter.js
@@ -0,0 +1,10 @@
+// API's
+// http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1
+// https://restcountries.eu/rest/v2/currency/cop
+
+// 1st function - getExchangeRate
+// 2nd function - getCountries
+// 3rd function - convertCurrency
+
+// Call convert currency to get meaningful data.
+
