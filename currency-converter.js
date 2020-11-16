ca4c1fb7fdf5a7144154aeeb5fa8bb4b03dcf2c0
Maxim Klochkov <m.klochkov@sap.com>
2020-11-16 19:02:46 +0300

    Completed functions

--- currency-converter.js
+++ currency-converter.js
@@ -5,22 +5,52 @@ const axios = require('axios');
 // https://restcountries.eu/rest/v2/currency/cop
 
 const API_CURRENCIES = 'http://data.fixer.io/api/latest?access_key=f68b13604ac8e570a00f7d8fe7f25e1b&format=1';
-const API_COUNTRIES = 'https://restcountries.eu/rest/v2/currency/cop';
+const API_COUNTRIES = 'https://restcountries.eu/rest/v2/currency';
 
 // 1st function - getExchangeRate
 const getExchangeRate = async (fromCurrency, toCurrency) => {
     const response = await axios.get(API_CURRENCIES);
 
     const rates = response.data.rates;
-    const euro = 1 / rates[fromCurrency];
-    const exchangeRate = euro * rates[toCurrency];
+    const base = 1 / rates[fromCurrency];
+    const exchangeRate = base * rates[toCurrency];
 
-    console.log(exchangeRate);
+    if (isNaN(exchangeRate)) {
+        throw new Error(`Unable to get currency ${fromCurrency} and ${toCurrency}`);
+    }
+
+    return exchangeRate;
 };
 
 // 2nd function - getCountries
+const getCountries = async (toCurrency) => {
+    try {
+        const response = await axios.get(`${API_COUNTRIES}/${toCurrency}`);
+
+        return response.data.map(country => country.name);
+    } catch(error) {
+        throw new Error(`Unable to get countries that use ${toCurrency}`);
+    }
+};
+
 // 3rd function - convertCurrency
+const convertCurrency = async (fromCurrency, toCurrency, amount) => {
+    const countries = await getCountries(toCurrency);
+    const exchangeRate = await getExchangeRate(fromCurrency, toCurrency);
+    const convertedAmount = (amount * exchangeRate).toFixed(2);
+
+    return `${amount} ${fromCurrency} is worth ${convertedAmount} ${toCurrency}. You can spend these in the following countries: ${countries}`;
+}
 
 // Call convert currency to get meaningful data.
+convertCurrency('USD', 'CAD', 30)
+    .then(response => {
+        console.log(response);
+    })
+    .catch(error => {
+        console.log(error.message);
+    })
+;
 
-getExchangeRate('EUR', 'RUB');
+// getExchangeRate('EUR', 'RUB');
+// getCountries('RUB');
